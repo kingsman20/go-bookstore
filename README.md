131b2fda86a16265abef9f41c66b278d8db2fe42
kingsleyObot <kingsley.obot@sastrify.com>
2022-09-11 08:15:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+### CRUD APIs in Goplang
+
+- GORM
+- MySQL
+- MUX
+
+### To test 
+
+- Setup a MySQL database and add credentials in config file
+- Pull repo 
+- cd to cmd > main
+- run `go build`
+- run `go run main.go`
---
+++ pkg/models/book.go
@@ -0,0 +1,48 @@
+package models
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	"github.com/kingsman20/go-bookstore/pkg/config"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:""json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	fmt.Println("Connected to the DB")
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book {
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
+}
